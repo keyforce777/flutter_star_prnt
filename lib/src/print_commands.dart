df50fc86c9baedc3edf616114807f319b27aa598
keyforce777 <165994511+keyforce777@users.noreply.github.com>
2026-01-03 20:36:32 +0200

    Update print_commands.dart

--- lib/src/print_commands.dart
+++ lib/src/print_commands.dart
@@ -1,109 +1,188 @@
-Resolving dependencies...
-Downloading packages...
-  _flutterfire_internals 1.3.59 (1.3.65 available)
-  archive 3.6.1 (4.0.7 available)
-  bloc 8.1.4 (9.2.0 available)
-  characters 1.4.0 (1.4.1 available)
-  device_info_plus 11.5.0 (12.3.0 available)
-  firebase_analytics 11.6.0 (12.1.0 available)
-  firebase_analytics_platform_interface 4.4.3 (5.0.5 available)
-  firebase_analytics_web 0.5.10+16 (0.6.1+1 available)
-  firebase_auth 5.7.0 (6.1.3 available)
-  firebase_auth_platform_interface 7.7.3 (8.1.5 available)
-  firebase_auth_web 5.15.3 (6.1.1 available)
-  firebase_core 3.15.2 (4.3.0 available)
-  firebase_core_web 2.24.1 (3.3.1 available)
-  flutter_bloc 8.1.6 (9.1.1 available)
-  flutter_blue_plus 1.36.8 (2.1.0 available)
-  flutter_blue_plus_android 7.0.4 (8.1.0 available)
-  flutter_blue_plus_darwin 7.0.3 (8.1.0 available)
-  flutter_blue_plus_linux 7.0.3 (8.1.0 available)
-  flutter_blue_plus_platform_interface 7.0.0 (8.1.0 available)
-  flutter_blue_plus_web 7.0.2 (8.1.0 available)
-  flutter_lints 3.0.2 (6.0.0 available)
-  fluttertoast 8.2.14 (9.0.0 available)
-  get_it 8.3.0 (9.2.0 available)
-  google_sign_in 6.3.0 (7.2.0 available)
-  google_sign_in_android 6.2.1 (7.2.7 available)
-  google_sign_in_ios 5.9.0 (6.2.4 available)
-  google_sign_in_platform_interface 2.5.0 (3.1.0 available)
-  google_sign_in_web 0.12.4+4 (1.1.0 available)
-! image 3.0.2 (overridden) (4.7.2 available)
-  lints 3.0.0 (6.0.0 available)
-  matcher 0.12.17 (0.12.18 available)
-  material_color_utilities 0.11.1 (0.13.0 available)
-  meta 1.16.0 (1.17.0 available)
-  mobile_scanner 6.0.11 (7.1.4 available)
-  petitparser 5.4.0 (7.0.1 available)
-  rxdart 0.27.7 (0.28.0 available)
-  screenshot 2.5.0 (3.0.0 available)
-  share_plus 11.1.0 (12.0.1 available)
-  sign_in_with_apple 6.1.4 (7.0.1 available)
-  sign_in_with_apple_platform_interface 1.1.0 (2.0.0 available)
-  sign_in_with_apple_web 2.1.1 (3.0.0 available)
-  sqlite3 2.9.4 (3.1.2 available)
-  test_api 0.7.6 (0.7.8 available)
-! xml 5.4.1 (overridden) (6.6.1 available)
-Got dependencies!
-44 packages have newer versions incompatible with dependency constraints.
-Try `flutter pub outdated` for more information.
-Launching lib\main.dart on 2406APNFAG in debug mode...
-Running Gradle task 'assembleDebug'...
-I/flutter (21074): fetchMyDevices Error: ClientException with SocketException: Failed host lookup: 'api.mfd057.kiev.ua' (OS Error: No address associated with hostname, errno = 7), uri=https://api.mfd057.kiev.ua/api_v2/get_my_devices.php
-I/flutter (21074): Error fetching requests: ClientException with SocketException: Failed host lookup: 'api.mfd057.kiev.ua' (OS Error: No address associated with hostname, errno = 7), uri=https://api.mfd057.kiev.ua/api_v2/get_incoming_requests.php
-I/flutter (21074): ClientException with SocketException: Failed host lookup: 'api.mfd057.kiev.ua' (OS Error: No address associated with hostname, errno = 7), uri=https://api.mfd057.kiev.ua/api_v2/check_pending_requests.php
-Set namespace for project device_info_plus to com.orderino.generated.device_info_plus
-Set namespace for project file_picker to com.orderino.generated.file_picker
-Set namespace for project file_saver to com.orderino.generated.file_saver
-Set namespace for project firebase_analytics to com.orderino.generated.firebase_analytics
-Set namespace for project firebase_auth to com.orderino.generated.firebase_auth
-Set namespace for project firebase_core to com.orderino.generated.firebase_core
-Set namespace for project flutter_blue_plus_android to com.orderino.generated.flutter_blue_plus_android
-Set namespace for project flutter_bluetooth_serial to com.orderino.generated.flutter_bluetooth_serial
-Set namespace for project flutter_file_dialog to com.orderino.generated.flutter_file_dialog
-Set namespace for project flutter_foreground_task to com.orderino.generated.flutter_foreground_task
-Set namespace for project flutter_native_contact_picker to com.orderino.generated.flutter_native_contact_picker
-Set namespace for project flutter_plugin_android_lifecycle to com.orderino.generated.flutter_plugin_android_lifecycle
-Set namespace for project flutter_pos_printer_platform to com.orderino.generated.flutter_pos_printer_platform
-Set namespace for project flutter_star_prnt to com.orderino.generated.flutter_star_prnt
-Set namespace for project fluttertoast to com.orderino.generated.fluttertoast
-Set namespace for project google_sign_in_android to com.orderino.generated.google_sign_in_android
-Set namespace for project mobile_number to com.orderino.generated.mobile_number
-Set namespace for project mobile_scanner to com.orderino.generated.mobile_scanner
-Set namespace for project package_info_plus to com.orderino.generated.package_info_plus
-Set namespace for project path_provider_android to com.orderino.generated.path_provider_android
-Set namespace for project permission_handler_android to com.orderino.generated.permission_handler_android
-Set namespace for project share_plus to com.orderino.generated.share_plus
-Set namespace for project shared_preferences_android to com.orderino.generated.shared_preferences_android
-Set namespace for project sign_in_with_apple to com.orderino.generated.sign_in_with_apple
-Set namespace for project smart_auth to com.orderino.generated.smart_auth
-Set namespace for project sqflite_android to com.orderino.generated.sqflite_android
-Set namespace for project sqlite3_flutter_libs to com.orderino.generated.sqlite3_flutter_libs
-Set namespace for project url_launcher_android to com.orderino.generated.url_launcher_android
-Set namespace for project wakelock_plus to com.orderino.generated.wakelock_plus
-/C:/Users/Admin/AppData/Local/Pub/Cache/git/flutter_star_prnt-b83141e51f398e6366f46d90327e2a5dd7184008/lib/src/print_commands.dart:206:36: Error: Undefined name 'pixelRatio'.
-          Size(logicalSize.width * pixelRatio, logicalSize.height * pixelRatio),
-                                   ^^^^^^^^^^
-/C:/Users/Admin/AppData/Local/Pub/Cache/git/flutter_star_prnt-b83141e51f398e6366f46d90327e2a5dd7184008/lib/src/print_commands.dart:206:69: Error: Undefined name 'pixelRatio'.
-          Size(logicalSize.width * pixelRatio, logicalSize.height * pixelRatio),
-                                                                    ^^^^^^^^^^
-/C:/Users/Admin/AppData/Local/Pub/Cache/git/flutter_star_prnt-b83141e51f398e6366f46d90327e2a5dd7184008/lib/src/print_commands.dart:209:27: Error: Undefined name 'pixelRatio'.
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_star_prnt/src/enums.dart';
+
+class PrintCommands {
+  List<Map<String, dynamic>> _commands = [];
+
+  List<Map<String, dynamic>> getCommands() => _commands;
+
+  appendEncoding(StarEncoding encoding) {
+    _commands.add({"appendEncoding": encoding.text});
+  }
+
+  appendCutPaper(StarCutPaperAction action) {
+    _commands.add({"appendCutPaper": action.text});
+  }
+
+  openCashDrawer(int actionNumber) {
+    _commands.add({"openCashDrawer": actionNumber});
+  }
+
+  appendBitmap({
+    required String path,
+    bool diffusion = true,
+    int width = 576,
+    bool bothScale = true,
+    int? absolutePosition,
+    StarAlignmentPosition? alignment,
+    StarBitmapConverterRotation? rotation,
+  }) {
+    final Map<String, dynamic> command = {
+      "appendBitmap": path,
+      "bothScale": bothScale,
+      "diffusion": diffusion,
+      "width": width,
+    };
+    if (absolutePosition != null) command['absolutePosition'] = absolutePosition;
+    if (alignment != null) command['alignment'] = alignment.text;
+    if (rotation != null) command['rotation'] = rotation.text;
+    _commands.add(command);
+  }
+
+  appendBitmapByte({
+    required Uint8List byteData,
+    bool diffusion = true,
+    int width = 576,
+    bool bothScale = true,
+    int? absolutePosition,
+    StarAlignmentPosition? alignment,
+    StarBitmapConverterRotation? rotation,
+  }) {
+    final Map<String, dynamic> command = {
+      "appendBitmapByteArray": byteData,
+      "bothScale": bothScale,
+      "diffusion": diffusion,
+      "width": width,
+    };
+    if (absolutePosition != null) command['absolutePosition'] = absolutePosition;
+    if (alignment != null) command['alignment'] = alignment.text;
+    if (rotation != null) command['rotation'] = rotation.text;
+    _commands.add(command);
+  }
+
+  appendBitmapWidget({
+    required BuildContext context,
+    required Widget widget,
+    bool diffusion = true,
+    int width = 576,
+    bool bothScale = true,
+    int? absolutePosition,
+    StarAlignmentPosition? alignment,
+    StarBitmapConverterRotation? rotation,
+    Duration? wait,
+    Size? logicalSize,
+    Size? imageSize,
+    TextDirection textDirection = TextDirection.ltr,
+  }) {
+    createImageFromWidget(
+      context,
+      widget,
+      wait: wait,
+      logicalSize: logicalSize,
+      imageSize: imageSize,
+      textDirection: textDirection,
+    ).then((byte) {
+      if (byte != null) {
+        appendBitmapByte(
+          byteData: byte,
+          diffusion: diffusion,
+          width: width,
+          bothScale: bothScale,
+          absolutePosition: absolutePosition,
+          alignment: alignment,
+          rotation: rotation,
+        );
+      }
+    });
+  }
+
+  appendBitmapText({
+    required String text,
+    int? fontSize,
+    bool diffusion = true,
+    int? width,
+    bool bothScale = true,
+    int? absolutePosition,
+    StarAlignmentPosition? alignment,
+    StarBitmapConverterRotation? rotation,
+  }) {
+    final Map<String, dynamic> command = {
+      "appendBitmapText": text,
+      "bothScale": bothScale,
+      "diffusion": diffusion,
+    };
+    if (fontSize != null) command['fontSize'] = fontSize;
+    if (width != null) command['width'] = width;
+    if (absolutePosition != null) command['absolutePosition'] = absolutePosition;
+    if (alignment != null) command['alignment'] = alignment.text;
+    if (rotation != null) command['rotation'] = rotation.text;
+    _commands.add(command);
+  }
+
+  push(Map<String, dynamic> command) => _commands.add(command);
+  clear() => _commands.clear();
+
+  static Future<Uint8List?> createImageFromWidget(
+    BuildContext context,
+    Widget widget, {
+    Duration? wait,
+    Size? logicalSize,
+    Size? imageSize,
+    TextDirection textDirection = TextDirection.ltr,
+  }) async {
+    final RenderRepaintBoundary repaintBoundary = RenderRepaintBoundary();
+    final ui.FlutterView flutterView = View.of(context);
+    final double pixelRatio = flutterView.devicePixelRatio;
+
+    final Size finalLogicalSize = logicalSize ?? (flutterView.physicalSize / pixelRatio);
+    final Size finalImageSize = imageSize ?? Size(
+      finalLogicalSize.width * pixelRatio,
+      finalLogicalSize.height * pixelRatio,
+    );
+
+    final RenderView renderView = RenderView(
+      view: flutterView,
+      child: RenderPositionedBox(
+        alignment: Alignment.center,
+        child: repaintBoundary,
+      ),
+      configuration: ViewConfiguration(
+        logicalConstraints: BoxConstraints.tight(finalLogicalSize),
+        physicalConstraints: BoxConstraints.tight(finalImageSize),
         devicePixelRatio: pixelRatio,
-                          ^^^^^^^^^^
-Target kernel_snapshot_program failed: Exception
+      ),
+    );
 
+    final PipelineOwner pipelineOwner = PipelineOwner();
+    final BuildOwner buildOwner = BuildOwner(focusManager: FocusManager());
 
-FAILURE: Build failed with an exception.
+    pipelineOwner.rootNode = renderView;
+    renderView.prepareInitialFrame();
 
-* What went wrong:
-Execution failed for task ':app:compileFlutterBuildDebug'.
-> Process 'command 'C:\src\flutter\bin\flutter.bat'' finished with non-zero exit value 1
+    final RenderObjectToWidgetElement<RenderBox> rootElement =
+        RenderObjectToWidgetAdapter<RenderBox>(
+      container: repaintBoundary,
+      child: Directionality(
+        textDirection: textDirection,
+        child: IntrinsicHeight(child: IntrinsicWidth(child: widget)),
+      ),
+    ).attachToRenderTree(buildOwner);
 
-* Try:
-> Run with --stacktrace option to get the stack trace.
-> Run with --info or --debug option to get more log output.
-> Run with --scan to get full insights.
-> Get more help at https://help.gradle.org.
+    buildOwner.buildScope(rootElement);
+    if (wait != null) await Future.delayed(wait);
+    buildOwner..buildScope(rootElement)..finalizeTree();
 
-BUILD FAILED in 8s
-Error: Gradle task assembleDebug failed with exit code 1
+    pipelineOwner..flushLayout()..flushCompositingBits()..flushPaint();
+
+    final ui.Image image = await repaintBoundary.toImage(
+      pixelRatio: finalImageSize.width / finalLogicalSize.width,
+    );
+
+    final ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    return byteData?.buffer.asUint8List();
+  }
+}
